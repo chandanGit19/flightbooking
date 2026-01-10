168ab6f1b2bd47e3fa78c451b24f179b324bc41a
Chandan kumar yadav <144602667+chandanGit19@users.noreply.github.com>
2026-01-11 00:23:47 +0530

    1. npx sequelize migration:generate --name    update-city-airport-association 2. sequelize provide many inbuild methods 3. city.createAirport

--- src/index.js
+++ src/index.js
@@ -14,7 +14,22 @@ app.get("/users", (req, res) => {
 
 app.use("/api", router)
 
-app.listen(serverConfig.PORT, ()=>{
+app.listen(serverConfig.PORT, async ()=>{
     console.log(`Server is running on the port: ${serverConfig.PORT}`);
     // logger.info("success fully ", "root", )
+
+    // add association from model for sequelixe
+
+    // const {city, Airport} = require("./models")
+
+    // const cityy = await city.findByPk(1)
+
+    // const airport = await Airport.create({name: "Kempegoda Airport", code: "BLR", cityId: 1})
+
+    // const kmpAirport = await cityy.createAirport({name: "Kempegoda Airport", code: "BLR", cityId: 1, address: "Banglore"})
+
+    // console.log("city -=-=--=-" , kmpAirport)
+
+
+
 })
